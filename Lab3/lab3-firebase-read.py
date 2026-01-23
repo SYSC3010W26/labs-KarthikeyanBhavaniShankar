a9735801b4df17a515287b809c3a780f3434beac
KarthikeyanBhavaniShankar <122071272+KarthikeyanBhavaniShankar@users.noreply.github.com>
2026-01-23 14:48:59 -0500

    Update lab3-firebase-read.py

--- Lab3/lab3-firebase-read.py
+++ Lab3/lab3-firebase-read.py
@@ -1,10 +1,6 @@
 # lab3-firebase-read.py
-# SYSC 3010 – Lab 3
-# Read data back from Firebase Realtime Database
-
 import pyrebase
 
-# Firebase configuration
 firebaseConfig = {
     "apiKey": "AIzaSyCfhpUXtm-FY6409LYRIKAheA8C1WasN88",
     "authDomain": "sysc3010-lab3-karthikeya-14579.firebaseapp.com",
@@ -13,20 +9,15 @@ firebaseConfig = {
     "storageBucket": "sysc3010-lab3-karthikeya-14579.appspot.com"
 }
 
-# Initialize Firebase
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
 db = firebase.database()
-
-# Anonymous login
 user = auth.sign_in_anonymous()
 
 print("Reading data from Firebase...\n")
 
-# Read data under your node
-data = db.child("karthikeyanpi").get(user["idToken"])
 
-# Print all entries
+data = db.child("karthikeyanpi").get(user["idToken"])
 for key, value in data.val().items():
     print("Entry ID:", key)
     print(value)

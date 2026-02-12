e08dd67758bb24c8e77dfb12059aa5f6c8ce10bd
Karthikeyan Bhavani Shankar <karthikeyanbhavanishankar@cmail.carleton.ca>
2026-02-12 14:11:30 -0500

    feat: Add new feature

--- Lab1/showsensordata.py
+++ Lab1/showsensordata.py
@@ -4,12 +4,25 @@ import time
 sense = SenseHat()
 sense.clear()
 
-while True:
+choice = input("Enter T for Temperature, H for Humidity, P for Pressure: ")
+choice = choice.upper()
+
+if choice == "T":
     temperature = sense.get_temperature()
-    pressure = sense.get_pressure()
+    sense.show_message(f"T:{temperature:.1f}C",
+                       text_colour=[255,0,0])
+
+elif choice == "H":
     humidity = sense.get_humidity()
+    sense.show_message(f"H:{humidity:.1f}%",
+                       text_colour=[0,0,255])
 
-    message = f"T:{temperature:.1f}C P:{pressure:.1f} H:{humidity:.1f}%"
-    sense.show_message(message, scroll_speed=0.07)
+elif choice == "P":
+    pressure = sense.get_pressure()
+    sense.show_message(f"P:{pressure:.1f}hPa",
+                       text_colour=[0,255,0])
 
-    time.sleep(1)
+else:
+    sense.show_message("Invalid Choice",
+                       text_colour=[255,255,0])
+

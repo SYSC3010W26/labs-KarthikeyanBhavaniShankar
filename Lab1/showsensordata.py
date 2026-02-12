b4f74692d8520e63cd0569a7db473c63a22822e4
Karthikeyan Bhavani Shankar <karthikeyanbhavanishankar@cmail.carleton.ca>
2026-02-12 13:56:34 -0500

    feat: Add new feature that expects you to enter a command

--- Lab1/showsensordata.py
+++ Lab1/showsensordata.py
@@ -4,12 +4,26 @@ import time
 sense = SenseHat()
 sense.clear()
 
-while True:
-    temperature = sense.get_temperature()
-    pressure = sense.get_pressure()
-    humidity = sense.get_humidity()
+choice = input("Which sensor to display: (t)emperature,(p)ressure, or (h)umidity: ")
+choice = choice.upper()
 
-    message = f"T:{temperature:.1f}C P:{pressure:.1f} H:{humidity:.1f}%"
-    sense.show_message(message, scroll_speed=0.07)
+if choice == "T":
+    temperature = sense.get_temperature()
+    sense.show_message(f"T:{temperature:.1f}C",
+                       text_colour=[255,0,0])
+
+elif choice == "H":
+    humidity = sense.get_humidity()
+    sense.show_message(f"H:{humidity:.1f}%",
+                       text_colour=[0,0,255])
+
+elif choice == "P":
+    pressure = sense.get_pressure()
+    sense.show_message(f"P:{pressure:.1f}hPa",
+                       text_colour=[0,255,0])
+
+else:
+    sense.show_message("Invalid Choice",
+                       text_colour=[255,255,0])
 
     time.sleep(1)
